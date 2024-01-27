952c9cc22fccc8e3edb5df475ffc86278378fe1b
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2024-01-27 23:20:23 +0530

    edit

--- app/Pages/Admin/contact/page.jsx
+++ app/Pages/Admin/contact/page.jsx
@@ -84,13 +84,20 @@ const AdminContactPage = () => {
   const handleDelete = async (contactId) => {
     try {
       setLoader(true)
-      const response = await fetch(`/api/contact/admin/delete/${contactId}`, {
-        method: "POST",
+      const res = await fetch(`/api/contact/delete/${contactId}`, {
+        method: "DELETE",
       });
-      if (!response.ok) {
-        throw new Error("Failed to delete contact form");
+      const data=await res.json();
+      console.log(data)
+      if(data.success===false){
         setLoader(false)
+        console.log(data.message);
       }
+      // if (!response.ok) {
+      //   console.log(response)
+      //   throw new Error("Failed to delete contact form");
+      //   setLoader(false)
+      // }
       // If deletion is successful, refetch the updated contact forms
       setLoader(false)
       fetchData();
