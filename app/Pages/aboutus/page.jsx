e84c95b332ad22028268b2a8a062c61f55763fa5
Shreekara B <shreekarab5@gmail.com>
2024-01-29 20:32:22 +0530

    About Kannada

--- app/Pages/aboutus/page.jsx
+++ app/Pages/aboutus/page.jsx
@@ -328,7 +328,7 @@ const page = () => {
         <div className="flex flex-col md:flex-row gap-2 lg:mb-10 lg:mt-10 w-full md:h-60 rounded-3xl p-2">
           <div className="w-full h-full md:w-1/2 p-2 gap-2 flex flex-col">
             <span className="text-lg md:text-2xl lg:text-4xl font-semibold text-center underline">
-              ಹೇಗೆ ಹೋಗಬಹುದು?
+            ತಲುಪುವುದು ಹೇಗೆ?
             </span>
             <p className="text-sm md:text-base lg:text-md text-center">
               <span className="block">
